1d0439b509cc0be492e6d8f77130cc560cd97741
Nils Martel <nilsmartel@yahoo.de>
2026-06-12 13:58:29 +0200

    Read stdin

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,10 @@ fn main() {
         eprintln!("expect first argument to be search key and string input via stdin");
         std::process::exit(0)
     };
+
+    let lines = std::io::stdin().lines();
+
+    for l in lines {
+    }
+}
 }
