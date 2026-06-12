e7cd60f37484e1acc29598490dff1b542558441d
Nils Martel <nilsmartel@yahoo.de>
2026-06-12 13:58:22 +0200

    argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let Some(key) = std::env::args().nth(1) else {
+        eprintln!("expect first argument to be search key and string input via stdin");
+        std::process::exit(0)
+    };
 }
